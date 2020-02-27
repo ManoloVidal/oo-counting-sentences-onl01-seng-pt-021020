f9b133fb47347e819d72f37bba1dd9c2ad6e422f
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-27 18:41:12 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -23,4 +23,3 @@ class String
 
   end
 end
-`require 'pry' class String def sentence? self.end_with?(".") end def question? self.end_with?("?") end def exclamation? self.end_with?("!") end def count_sentences sent_array = self.split(/[.?!]+/) sent_array.count end end`
